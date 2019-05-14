ae9db1e6242f3204f95488c50eca9d82e9113c4d
VillainousVelociraptor <gutierrezchristianf@gmail.com>
2019-05-14 00:00:56 -0700

    Added javascript color logo teal on hover

--- js/index.js
+++ js/index.js
@@ -7,10 +7,11 @@ function hover(myLogo) {
 function unhover(myLogo) {
   myLogo.setAttribute('src', '/images/Virtual_Velociraptor_Logo_White.png');
 }
-
-/*function hover(myLogo) {  myLogo.setAttribute('src','https://previews.dropbox.com/p/thumb/AAakhd0mcbhy1YJAtnxe4tnGu2bkdgsyyd1w21kj7doXa18laVVUYMcYnw0rjFqNOR21leB3LceZg8KRpQlhyTtocR3TXf92mntzi8brKDf2KcIOQS2C-3abdH-dPMjE50OB9ZiAOI6LZYGfI6xo6quabZw_gBwBjIHLkZmjruiZzgg6AySAOMSirBwn4Xd7JZTnOdRp8cRH0QfEpfynDYOGjRqmNQapN2cagfSroL5MLmqFCk_NCRoQBx887rcBbTVqSXz-CsFnv9rnDKw31oBCS6Ewqwuixi7M_k77g1ontLbXi1eshJV9bi8RNkIj8PejE6Ln6ra8477Ai381VsYb/p.png?fv_content=true&size_mode=5');
+/*
+function hover(myLogo) {  myLogo.setAttribute('src','https://previews.dropbox.com/p/thumb/AAakhd0mcbhy1YJAtnxe4tnGu2bkdgsyyd1w21kj7doXa18laVVUYMcYnw0rjFqNOR21leB3LceZg8KRpQlhyTtocR3TXf92mntzi8brKDf2KcIOQS2C-3abdH-dPMjE50OB9ZiAOI6LZYGfI6xo6quabZw_gBwBjIHLkZmjruiZzgg6AySAOMSirBwn4Xd7JZTnOdRp8cRH0QfEpfynDYOGjRqmNQapN2cagfSroL5MLmqFCk_NCRoQBx887rcBbTVqSXz-CsFnv9rnDKw31oBCS6Ewqwuixi7M_k77g1ontLbXi1eshJV9bi8RNkIj8PejE6Ln6ra8477Ai381VsYb/p.png?fv_content=true&size_mode=5');
 }
 
 function unhover(myLogo) {
-  myLogo.setAttribute('src', 'https://previews.dropbox.com/p/thumb/AAZI8NwVJWocjqmy8B7vNVGKndSEqWTGSvi3-grsCe_54ZACS3WUH3DIN5J7dGVf1kOA91FQz5G0oSGy7kUYY4f2OHu-omQNSSLJkuVi9dey3xxuCQpEerDe7roG_L13ZuDybnLYM1Kt96OEfEbcprzrzk74JZDCEmjCtgM55kjZl6enpDCzDJ2meHKxvsoI_dSxlxZq7UhX6LHhMn607i2gByBGRbeegkW4sZr4hRmvLfM24HZJgFrY2GCHKu5jsvFFFfdxjKNG5ZDDDHoXaiu96-OmKatwTe0zGwf3IV7OPJj4bNCgT3X5fvSHJskVLzDPb_0fT1y87j0EBqbzjC3n/p.png?fv_content=true&size_mode=5');
-}*/
+  myLogo.setAttribute('src', 'https://previews.dropbox.com/p/thumb/AAbRMftbEX9ugf7JXKy53Jga7YZuIB4SX_fb-QfFNqP3NfcKVp8SOjWxubeJSHXyq9P_j9NWYZ9D9j7J0OC8yMZdxFaIs52VvkSWpAoU7hQ7VvgYvF_zqNike3CD03PNVZ8bhI9lX-5DFvltcxYTz965Uy_J9cnMHliy__Z7Vh1Lptvf_aYfxZIPua3KgzTUUypHXRMOGO5215oqAXtuix3RmjWY6GXODZViCxVrDIm2tIArp7S62y3WqXIqjg2NOM1WWElnjsMQ_hgm0h5ff5OuSO78Xgi63hVJHtnoTjSBbAD-bkI2v8E2fLQRczeZyTb2ZjHK3ex1WvenKXXXQqR_/p.png?fv_content=true&size_mode=5');
+}
+*/

2cabeb1c43ce3518197c2c77992c0cda819074d2
VillainousVelociraptor <gutierrezchristianf@gmail.com>
2019-05-13 23:31:00 -0700

    Logo color change on hover

---
+++ js/index.js
@@ -0,0 +1,16 @@
+var myLogo = document.getElementById('vv-logo');
+
+function hover(myLogo) {
+  myLogo.setAttribute('src','/images/Virtual_Velociraptor_Logo_Teal.png');
+}
+
+function unhover(myLogo) {
+  myLogo.setAttribute('src', '/images/Virtual_Velociraptor_Logo_White.png');
+}
+
+/*function hover(myLogo) {  myLogo.setAttribute('src','https://previews.dropbox.com/p/thumb/AAakhd0mcbhy1YJAtnxe4tnGu2bkdgsyyd1w21kj7doXa18laVVUYMcYnw0rjFqNOR21leB3LceZg8KRpQlhyTtocR3TXf92mntzi8brKDf2KcIOQS2C-3abdH-dPMjE50OB9ZiAOI6LZYGfI6xo6quabZw_gBwBjIHLkZmjruiZzgg6AySAOMSirBwn4Xd7JZTnOdRp8cRH0QfEpfynDYOGjRqmNQapN2cagfSroL5MLmqFCk_NCRoQBx887rcBbTVqSXz-CsFnv9rnDKw31oBCS6Ewqwuixi7M_k77g1ontLbXi1eshJV9bi8RNkIj8PejE6Ln6ra8477Ai381VsYb/p.png?fv_content=true&size_mode=5');
+}
+
+function unhover(myLogo) {
+  myLogo.setAttribute('src', 'https://previews.dropbox.com/p/thumb/AAZI8NwVJWocjqmy8B7vNVGKndSEqWTGSvi3-grsCe_54ZACS3WUH3DIN5J7dGVf1kOA91FQz5G0oSGy7kUYY4f2OHu-omQNSSLJkuVi9dey3xxuCQpEerDe7roG_L13ZuDybnLYM1Kt96OEfEbcprzrzk74JZDCEmjCtgM55kjZl6enpDCzDJ2meHKxvsoI_dSxlxZq7UhX6LHhMn607i2gByBGRbeegkW4sZr4hRmvLfM24HZJgFrY2GCHKu5jsvFFFfdxjKNG5ZDDDHoXaiu96-OmKatwTe0zGwf3IV7OPJj4bNCgT3X5fvSHJskVLzDPb_0fT1y87j0EBqbzjC3n/p.png?fv_content=true&size_mode=5');
+}*/
